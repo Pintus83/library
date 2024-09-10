0c8d197cf8e05df97c0b6aa91c44b3ff574bc606
Pintus83 <pintus83@gmail.com>
2024-09-10 18:05:28 +0100

    added two methods for the Customer class

--- library.rb
+++ library.rb
@@ -19,6 +19,27 @@ class Customer
     @name = name
     @borr_book = []
   end
+
+  def borrow(book)
+    if book.copies > 0
+      @borr_book << book
+      book.copies -= 1
+      puts "#{name} is borrowing: #{book.title}"
+    else
+      puts "Sorry, we don't have enough copies of this book."
+    end
+  end
+
+  def take_back(book)
+    if @borr_book.include?(book)
+      @borr_book.delete(book)
+      book.copies += 1
+      puts "#{name} took back this book: #{book.title}"
+    else
+      puts "#{name}, you didin't borrow this book"
+    end
+  end
+
 end
 
 # define a class for the library
